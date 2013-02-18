83d5e7d386c89ce857b19f371faf6ffb64df80fc
David <dck2001@med.cornell.edu>
2013-02-18 16:15:03 -0500

    Parser prints correct output; takes in path

--- parser.py
+++ parser.py
@@ -1,19 +1,24 @@
-import csv,re,textwrap
-with open('test.csv', "rU") as csvfile:
+import csv,re,textwrap,sys
+inputFile = sys.argv
+with open(inputFile[1], "rU") as csvfile:
     fastaname = csvfile.name.split(".")[0]
-    fastaoutput = open(fastaname + ".fasta", 'w')
     filereader = csv.reader(csvfile, delimiter=',')
+    rowcount = 0 
+    output = open(fastaname + ".fasta", 'w')
     for row in filereader:
-        if any("NC_" in s for s in row):
+	if any("NC_" in s for s in row):
                 header = ">" + row[0]
-                rowcount = 1
-        elif(rowcount ==1):
-            if any("Length" in s for s in row):
-                name = row[0].strip()
-        elif(rowcount == 2):
-            rowHeader = header + "|" + row[1] + "|" + row[2] + "|" + name
-            outputstring += rowHeader
-            outputstring += "\n".join(textwrap.wrap(row[0], 80))
-     fastaoutput.write(outputstring)
-    fastaoutput.close()
+		rowcount = 1
+		print("header initialized")
+	elif(rowcount == 1):
+	    if any("Start" in s for s in row):
+		name = ''.join(row[0].split())
+		print("name initialized")
+		rowcount = 2
+	else:
+                if (len(row[0]) != 0):
+                    rowHeader = header + "|" + row[1] + "|" + row[2] + "|" + name
+                    print rowHeader
+                    print ('\n'.join(textwrap.wrap(row[0], 80)))
+
 csvfile.close()
